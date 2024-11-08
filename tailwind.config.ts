b8efc3e8c2b0ea2012c5ec51a44900974551c480
Jason Christopher <jason.christopher@u.nus.edu>
2024-11-09 01:12:32 +0800

    feat: add tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,9 +9,37 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        white: "#FFFFFF",
+        navy: "#072446",
+        black: "#000000",
+        grey: {
+          100: "#E0E0E0",
+          300: "#B7B7B7",
+          500: "#585858"
+        },
+        green: {
+          100: "#DBFFE1",
+          500: "#78C142",
+        },
+        orange: {
+          100: "#FFF6D0",
+          500: "#FF8800",
+        },
+        red: {
+          100: "#FFF0F0",
+          500: "#FA646A"
+        },
+        blue: {
+          500: "#1B9DDF"
+        },
+        yellow: {
+          500: "#FAB900"
+        }
       },
+      fontFamily: {
+        heading: ["Hanken Grotesk", "sans-serif"],
+        sans: ["Lato", "sans-serif"]
+      }
     },
   },
   plugins: [],

0fd9e445e8805759b4907c98c06ae08545d1b3a0
Jason Christopher <jason.christopher@u.nus.edu>
2025-01-02 04:12:55 +0800

    UI updates

---
+++ src/app/styles.ts
@@ -0,0 +1,10 @@
+const styles = {
+    "heading1": "font-heading font-bold text-[24px]",
+    "heading2": "font-heading font-bold text-[20.3px]",
+    "heading3": "font-heading font-bold text-[16.2px]",
+    "heading4": "font-heading font-bold text-[14px]",
+    // "heading5": "font-heading font-bold text-[22px]",
+
+}
+
+export default styles

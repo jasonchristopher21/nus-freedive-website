da03b10d4d9f6d20fe7788065a38624dd04b6e5f
Jason Christopher <jason.christopher@u.nus.edu>
2025-02-27 13:55:45 +0800

    Fix schema to connect with Auth Users

---
+++ prisma/migrations/20250227054826_link_user_to_auth/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `dummy` on the `User` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "dummy";
